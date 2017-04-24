d089b409acebf7be120ebfe611721a8fc292a80e
Hankyu Jang <jhkmath@gmail.com>
2017-04-24 18:36:38 -0400

    Fixed bug when creating data. Now it corretly creates data using cells as samples and genes as features. Also changed the format of the dataset being saved. Now the npz format data has X_train, y_train, X_test, y_test, Kfold

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -6,45 +6,60 @@ import sklearn.model_selection
 import sklearn.metrics
 
 def get_X_y(infile):
-    # Skip irrelevant lines
-    for i in range(7):
-        infile.readline()
-    header = infile.readline()
-    header = header.split()
+    # Skip 'tissue' (not using in the classification)
+    infile.readline()
+    # Use group labels here for the labels
+    group = infile.readline().split()
+    y = np.array(group[2:]).astype(int)
 
+    # Skip lines not used in the classification
+    for i in range(5):
+        infile.readline()
+
+    # Get cell ids
+    cell_id = infile.readline().split()
+    header = np.array(cell_id[2:])
+
+    # Skip lines not used in the classification
     for i in range(3):
         infile.readline()
 
     X = np.zeros((4998, 3005)).astype(int)
-    y = np.zeros(4998).astype(int)
 
+    # Save the data in X matrix (each column is the vector of one cell with gene expressions)
     for i in range(4998):
-        line = infile.readline()
-        line = line.split()
-        y[i] = int(line[1])
-        X[i] = [int(value) for value in line[2:]]
+        line = infile.readline().split()
+        X[i] = line[2:]
 
+    # Read the rest of files
     for line in infile:
         continue
 
-    return X, y
+    return X.T, y
 
 def prepare_train_test_using_kfold(k, X, y):
     trainset, testset = [], []
     kf = sklearn.model_selection.KFold(n_splits=k, shuffle=True)
     kf.get_n_splits(X)
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
     print(kf)
     for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        trainset.append((X_train, y_train))
-        testset.append((X_test, y_test))
-    return trainset, testset
+        # X_train, X_test = X[train_index], X[test_index]
+        # y_train, y_test = y[train_index], y[test_index]
+        
+        X_train.append(X[train_index])
+        y_train.append(y[train_index])
+        X_test.append(X[test_index])
+        y_test.append(y[test_index])
+    return X_train, y_train, X_test, y_test
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="gets txt file format input, then classify single cells into 9 types")
     parser.add_argument('-i', '--infile', type=argparse.FileType('r'), 
-            help="txt file format (line 8: header, line 12 ~ line5009: data - col1: name - col2: label col3 ~ col: activation levels)", default=sys.stdin)
+            help="Use expressionmRNAAnnotations.txt", default=sys.stdin)
     parser.add_argument('-o', '--outfile', help="output file name")
     parser.add_argument('-kfold', '--kfold', type=int, default=5, help="k-fold (positive integer. default is 5-fold)")
     args = parser.parse_args()
@@ -58,9 +73,9 @@ if __name__ == '__main__':
     #####################################################################
     # Get trainsets and testsets using K-Fold
     print("Preparing the trainsets and testsets using {} fold...".format(k_fold))
-    trainset, testset = prepare_train_test_using_kfold(k_fold, X, y)
+    X_train, y_train, X_test, y_test = prepare_train_test_using_kfold(k_fold, X, y)
 
-    np.savez(args.outfile, Train = trainset, Test = testset, Kfold = k_fold)
+    np.savez(args.outfile, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, Kfold = k_fold)
     print("-"*60)
     print("trainset and testset are created!")
     print("They are saved as `dataset.npz` in the same directory")
